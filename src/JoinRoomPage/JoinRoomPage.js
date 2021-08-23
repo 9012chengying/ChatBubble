d5529312836d0f58744356c84820522126186de4
Ziyu Liu <LiuZ71@cardiff.ac.uk>
2021-08-23 18:52:52 +0100

    add JoinPage,and some other functional buttons

--- src/JoinRoomPage/JoinRoomPage.js
+++ src/JoinRoomPage/JoinRoomPage.js
@@ -1,13 +1,43 @@
-import React from 'react';
-import './JoinRoomPage.css';
+import React , {useEffect} from "react";
+import {connect} from "react-redux";
+import "./JoinRoomPage.css";
+import {setIsRoomHost} from "../store/actions";
+import { useLocation } from 'react-router-dom';
+import JoinRoomTitle from "./JoinRoomTitle";
+import JoinRoomContent from "./JoinRoomContent";
 
+const JoinRoomPage = (props) => {
+    const { setIsRoomHostAction, isRoomHost } = props;
 
-const JoinRoomPage = () => {
-    return (
-        <div>
-        JoinRoomPage
+    const search = useLocation().search;
+
+    useEffect(() =>{
+        const isRoomHost = new URLSearchParams(search).get('host');
+        if (isRoomHost){
+            //change information about it in our store
+            setIsRoomHostAction(true);
+        }
+
+    }, []);
+
+    return <div className='join_room_page_container'>
+        <div className='join_room_page_panel'>
+            <JoinRoomTitle isRoomHost={isRoomHost}/>
+            <JoinRoomContent />
         </div>
-    );
+    </div>;
 };
 
-export default JoinRoomPage;
+const mapStoreStateToProps = (state) => {
+    return {
+        ...state,
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        setIsRoomHostAction: (isRoomHost) => dispatch(setIsRoomHost())
+    }
+}
+
+export default connect(mapStoreStateToProps,mapDispatchToProps)(JoinRoomPage);
