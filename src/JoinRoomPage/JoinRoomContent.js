2716314be04a36b4ded0767ecb1173f8bd82acfc
Ziyu Liu <LiuZ71@cardiff.ac.uk>
2021-08-23 21:34:45 +0100

    join room as a host

--- src/JoinRoomPage/JoinRoomContent.js
+++ src/JoinRoomPage/JoinRoomContent.js
@@ -1,20 +1,40 @@
 import React , {useState} from 'react';
 import {connect} from "react-redux";
 import JoinRoomInputs from "./JoinRoomInputs";
-import {setConnectOnlyWithAudio} from "../store/actions";
+import {
+        setConnectOnlyWithAudio,
+        setIdentity,
+        setRoomId
+        } from "../store/actions";
 import OnlyWithAudioCheckbox from "./OnlyWithAudioCheckbox";
 import RoomNotFoundMessage from "./RoomNotFoundMessage";
 import JoinRoomButtons from "./JoinRoomButtons";
+import {useHistory} from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
 
 const JoinRoomContent = (props) => {
-    const { isRoomHost,setConnectOnlyWithAudioAction, connectOnlyWithAudio} =
- props;
+    const {
+        isRoomHost,
+        setConnectOnlyWithAudioAction,
+        connectOnlyWithAudio,
+        setRoomIdAction,
+        setIdentityAction,
+    } = props;
+
     const [roomIdValue, setRoomIdValue] = useState("");
     const [nameValue, setNameValue] = useState("");
     const [showRoomNotFoundMessage, setShowRoomNotFoundMessage] = useState(false);
 
-    const handleJoinToRoom = () =>{
-        //add logic to join a room
+        const history = useHistory();
+
+    const handleJoinToRoom = async () =>{
+        setIdentity(nameValue);
+        if (!isRoomHost) {
+            //check if room exists and if yes join
+        } else{
+            setRoomIdAction(uuidv4());
+            history.push('/room');
+        }
     };
 
     return (
@@ -41,10 +61,12 @@ const JoinRoomContent = (props) => {
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        setConnectOnlyWithAudioAction:(onlyWithAudio) =>
-            dispatch(setConnectOnlyWithAudio(onlyWithAudio))
+        setConnectOnlyWithAudioAction: (onlyWithAudio) =>
+            dispatch(setConnectOnlyWithAudio(onlyWithAudio)),
+        setIdentityAction: (identity) => dispatch(setIdentity(identity)),
+        setRoomIdAction: (id) => dispatch(setRoomId(id))
     };
-}
+};
 
 const mapStoreStateToProps = (state) => {
     return {
