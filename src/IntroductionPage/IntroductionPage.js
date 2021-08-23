c87c8e284dd1887e712e34df2ae10f4e7f13be59
Ziyu Liu <LiuZ71@cardiff.ac.uk>
2021-08-23 18:23:39 +0100

    add IntroductionPage.js, can jump to different pages

--- src/IntroductionPage/IntroductionPage.js
+++ src/IntroductionPage/IntroductionPage.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './IntroductionPage.css';
 import logo from '../resources/images/logo.png'
+import ConnectingButtons from "./ConnectingButtons";
+
 
 const IntroductionPage = () => {
     return (
         <div className='introduction_page_container'>
             <div className='introduction_page_panel'>
                 <img src={logo} className='introduction_page_image' />
+                <ConnectingButtons />
             </div>
         </div>
     );
 };
 
 export default IntroductionPage;
+
+
