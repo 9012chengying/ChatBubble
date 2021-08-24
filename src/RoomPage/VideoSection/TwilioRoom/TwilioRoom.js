a6d46d49c4eecb4ee574da688ddca5ab93c11c64
Mathew <c2108425@cardiff.ac.uk>
2021-08-25 00:18:10 +0100

    Adding and removing participants from store to display later. Cant test atm as something else needs to be updated first. sets state for the participants as well.

--- src/RoomPage/VideoSection/TwilioRoom/TwilioRoom.js
+++ src/RoomPage/VideoSection/TwilioRoom/TwilioRoom.js
@@ -21,6 +21,16 @@ class TwilioRoom extends Component {
         });
     }
 
+    componentDidMount() {
+        this.props.room.on('participantConnected', (participant) =>
+            this.addParticipant(participant)
+        );
+
+        this.props.room.on('participantDisconnected' , (participant) => {
+            this.removeParticipant(participant);
+        });
+    }
+
     //Adding a participant within the room to the store.
     //Only if not already there.
     //Else is added/pushed to the store as a new participant.
@@ -35,4 +45,41 @@ class TwilioRoom extends Component {
         };
 
     }
+
+    //Removing participants from store when they leave.
+    //Checks to find the participant to remove.
+    removeParticipantFromStore(participant) {
+        const participants = store.getState().participants.filter(p => p.identity === participant.identity);
+        store.dispatch(setParticipants(participants));
+    }
+
+    //When participant join will console log the person joining.
+    //Will set state for that person.
+    addParticipant(participant) {
+        console.log('${participant.identity} has joined the room');
+        this.addParticipantToStore(participant);
+
+        this.setState({
+            remoteParticipants: [...this.state.remoteParticipants, participant],
+        });
+    }
+
+    //When leaving will set state for that person.
+    //Again loops through to find correct person.
+    removeParticipant(participant) {
+        console.log('${participant.identity} has left the room');
+        this.removeParticipantFromStore(participant);
+        this.setState({
+            remoteParticipants: this.state.remoteParticipants.filter((p) => p.identity !== participant.identity),
+        })
+    }
+
+    //render page.
+    render() {
+        return(
+            <div>Participants</div>
+        )
+    }
 }
+
+export default TwilioRoom;
